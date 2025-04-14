2bda5cd355c5ee90f5d6ade1c0d130a02b11a48e
tartaric_acid <baka943@qq.com>
2025-04-14 18:55:49 +0800

    首次更新

--- src/main/java/com/github/tartaricacid/simplebedrockmodel/client/bedrock/pojo/BedrockVersion.java
+++ src/main/java/com/github/tartaricacid/simplebedrockmodel/client/bedrock/pojo/BedrockVersion.java
@@ -1,65 +1,46 @@
 package com.github.tartaricacid.simplebedrockmodel.client.bedrock.pojo;
 
 
-import cn.sh1rocu.simplebedrockmodel.client.SimpleBedrockModelClient;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
-import net.fabricmc.loader.api.metadata.version.VersionPredicate;
+import com.github.tartaricacid.simplebedrockmodel.SimpleBedrockModel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 
-@Environment(EnvType.CLIENT)
+@OnlyIn(Dist.CLIENT)
 public enum BedrockVersion {
     /**
      * 旧版本基岩版模型，仅限 1.10.0
      */
-    LEGACY("1.10.0"),
+    LEGACY("[1.10.0]"),
     /**
      * 新版本基岩版模型，往后的 1.14.0，1.16.0 1.21.0 通通用此版本读取
      */
-    NEW(">=1.12.0");
+    NEW("[1.12.0,)");
 
-    private final VersionPredicate versionRange;
+    private final VersionRange versionRange;
 
     BedrockVersion(String version) {
         this.versionRange = createFromVersionSpec(version);
     }
 
     public static boolean isNewVersion(BedrockModelPOJO bedrockModel) {
-        Version inputVersion;
-        try {
-            inputVersion = Version.parse(bedrockModel.getFormatVersion());
-        } catch (VersionParsingException e) {
-            throw new RuntimeException(e);
-        }
-        return NEW.versionRange.test(inputVersion);
+        DefaultArtifactVersion inputVersion = new DefaultArtifactVersion(bedrockModel.getFormatVersion());
+        return NEW.versionRange.containsVersion(inputVersion);
     }
 
     public static boolean isLegacyVersion(BedrockModelPOJO bedrockModel) {
-        Version inputVersion;
-        try {
-            inputVersion = Version.parse(bedrockModel.getFormatVersion());
-        } catch (VersionParsingException e) {
-            throw new RuntimeException(e);
-        }
-        return LEGACY.versionRange.test(inputVersion);
+        DefaultArtifactVersion inputVersion = new DefaultArtifactVersion(bedrockModel.getFormatVersion());
+        return LEGACY.versionRange.containsVersion(inputVersion);
     }
 
-    private static VersionPredicate createFromVersionSpec(final String spec) {
+    private static VersionRange createFromVersionSpec(final String spec) {
         try {
-            return VersionPredicate.parse(spec);
-        } catch (VersionParsingException e) {
-            SimpleBedrockModelClient.LOGGER.fatal("Failed to parse version spec {}", spec, e);
+            return VersionRange.createFromVersionSpec(spec);
+        } catch (InvalidVersionSpecificationException e) {
+            SimpleBedrockModel.LOGGER.fatal("Failed to parse version spec {}", spec, e);
             throw new RuntimeException("Failed to parse spec", e);
         }
-    }
-
-    public static BedrockVersion getVersion(BedrockModelPOJO pojo) {
-        if (isNewVersion(pojo)) {
-            return NEW;
-        } else if (isLegacyVersion(pojo)) {
-            return LEGACY;
-        }
-        throw new RuntimeException("Invalid version for model: " + pojo);
     }
 }
