f3c4ae2ed0cc9f9b377fdad174c9965c1a26c415
286799714 <41233300+286799714@users.noreply.github.com>
2025-07-11 23:48:26 +0800

    移除不需要的项目

--- src/main/java/com/github/tartaricacid/simplebedrockmodel/SimpleBedrockModel.java
+++ src/main/java/com/github/tartaricacid/simplebedrockmodel/SimpleBedrockModel.java
@@ -1,13 +1,6 @@
 package com.github.tartaricacid.simplebedrockmodel;
 
-import com.github.tartaricacid.simplebedrockmodel.example.init.ModBlockEntityTypes;
-import com.github.tartaricacid.simplebedrockmodel.example.init.ModBlocks;
-import com.github.tartaricacid.simplebedrockmodel.example.init.ModCreativeModeTabs;
-import com.github.tartaricacid.simplebedrockmodel.example.init.ModItems;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,10 +10,5 @@ public class SimpleBedrockModel {
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public SimpleBedrockModel() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModItems.ITEMS.register(eventBus);
-        ModBlocks.BLOCKS.register(eventBus);
-        ModBlockEntityTypes.BLOCK_ENTITY_TYPE.register(eventBus);
-        ModCreativeModeTabs.CREATIVE_MODE_TABS.register(eventBus);
     }
 }
